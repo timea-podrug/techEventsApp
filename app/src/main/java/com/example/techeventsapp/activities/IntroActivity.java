9597c67e035d6f925e732cf5778be35db4bd4de4
Timea2 <timeapodrug@gmail.com>
2023-05-25 08:22:04 +0200

    intro activity

---
+++ app/src/main/java/com/example/techeventsapp/activities/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.techeventsapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.techeventsapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+    private Button startBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initView();
+    }
+
+    private void initView() {
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this,Main2Activity.class)));
+    }
+}
--- app/src/main/java/com/example/techeventsapp/activities/Main2Activity.java
+++ app/src/main/java/com/example/techeventsapp/activities/Main2Activity.java
@@ -1,14 +1,16 @@
-package com.example.techeventsapp;
+package com.example.techeventsapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.techeventsapp.R;
+
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
